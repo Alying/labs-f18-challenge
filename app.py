0c644fad425d134e7adc7a063b599d2cfe9444dd
Alina Ying <alina.ying@gmail.com>
2018-04-20 18:19:30 -0400

    Finished program.

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def main():
+@app.route('/')
+def index():
     return render_template('index.html')
 
+@app.route('/pokemon/<id_or_name>')
+def pokemon(id_or_name):
+	try:
+		print int(id_or_name)
+		return "You found a " + requests.get('https://pokeapi.co/api/v2/pokemon/' + id_or_name).json()['name'] + "!"
+	except ValueError:
+		return "The ID number is: "+ str(requests.get('https://pokeapi.co/api/v2/pokemon/' + id_or_name).json()['id'])
 
 if __name__ == '__main__':
     app.run()
